930aa16e2225f9b21df39f55f8e4ad6a66833c10
Sourav Mansingh <somansin@cisco.com>
2024-01-05 19:05:40 +0530

    [Portfolio] Initial Commit

---
+++ src/data/constants.js
@@ -0,0 +1,271 @@
+export const Bio = {
+    name: 'Sourav Snigdha Mansingh',
+    roles: [
+        "Full Stack Developer",
+        "Backend Developer"
+    ],
+    description: "",
+    github:"",
+    resume:"",
+    linkedin:"",
+    twitter:"",
+    medium:""
+};
+
+export const skills = [
+    {
+        title: "Frontend",
+        skills: [
+            {
+                name: "React JS",
+                image: ""
+            },
+            {
+                name: "Redux",
+                image: ""
+            },
+            {
+                name: "NextJS",
+                image: ""
+            },
+            {
+                name: "Vanilla JS",
+                image: ""
+            },
+            {
+                name: "LIT-HTML",
+                image: ""
+            },
+            {
+                name: "Tailwind",
+                image: ""
+            },
+            {
+                name: "TypeScript",
+                image: ""
+            }
+        ]
+    },
+    {
+        title: "Backend",
+        skills: [
+            {
+                name: "Java",
+                image:""
+            },
+            {
+                name: "NodeJS",
+                image: ""
+            },
+            {
+                name: "Kotlin",
+                image: ""
+            },
+            {
+                name: "Python",
+                image: ""
+            },
+            {
+                name: "Rust",
+                image: ""
+            }
+        ]
+    },
+    {
+        title:"Database",
+        skills:[
+            {
+                name: "MySQL",
+                image: ""
+            },
+            {
+                name: "MongoDB",
+                image: ""
+            },
+            {
+                name: "PostgreSQL",
+                image: ""
+            },
+            {
+                name: "Redis",
+                image: ""
+            },
+            {
+                name: "Cassandra",
+                image: ""
+            },
+            {
+                name: "RocksDB",
+                image: ""
+            },
+            {
+                name: "DynamoDB",
+                image: ""
+            }
+        ]
+    },
+    {
+        title: "Framework",
+        skills: [
+            {
+                name: "SpringBoot",
+                image: ""
+            },
+            {
+                name: "Ktor",
+                image: ""
+            },
+            {
+                name: "Flask",
+                image: ""
+            },
+            {
+                name: "Spring Security",
+                image: ""
+            },
+            {
+                name: "GraphQL",
+                image: ""
+            },
+            {
+                name: "Kite",
+                image: ""
+            },
+            {
+                name: "JWT",
+                image: ""
+            },
+            {
+                name: "OAuth2.0",
+                image: ""
+            },
+        ]
+    },
+    {
+        title: "Testing Frameworks",
+        skills: [
+            {
+                name: "Mockito",
+                image: ""
+            },
+            {
+                name: "JasmineJS",
+                image: ""
+            },
+            {
+                name: "Mocha",
+                image: ""
+            },
+            {
+                name: "K6",
+                image: ""
+            }
+        ]
+    },
+    {
+        title: "Messaging Queues",
+        skills: [
+            {
+                name: "Apache Kafka",
+                image: ""
+            },
+            {
+                name: "RabbitMQ",
+                image: ""
+            }
+        ]
+    },
+    {
+        title: "DevOps",
+        skills: [
+            {
+                name: "Docker",
+                image: ""
+            },
+            {
+                name: "Kubernetes",
+                image: ""
+            },
+            {
+                name: "Jenkins",
+                image: ""
+            },
+            {
+                name: "Amazon AWS",
+                image: ""
+            },
+            {
+                name: "GCP",
+                image: ""
+            },
+            {
+                name: "IBM PCF",
+                image: ""
+            }
+
+        ]
+    },
+];
+
+export const experience = [
+    {
+        id: 0,
+        img: "",
+        name: "CISCO",
+        duration: "2021-Present",
+        desingation: "SDE-III",
+        description: "Transforming the legacy application into scalable microservice based application which would have low latency and high throughput"
+    },
+    {
+        id: 1,
+        img: "",
+        name: "DBS Bank - DAH2",
+        duration: "2020-2021",
+        desingation: "Specialist Web Application Developer",
+        description: "Created web applications for Middle Office Technology(MOT) in the Risk Management Domain. This applications used workflows like camunda and BPM highly. For better performance the web applciation was implemented using LIT-HTML that enabled us to bind different web components in a single page."
+    },
+    {
+        id: 2,
+        img: "",
+        name: "Atos-Syntel",
+        duration: "2018-2020",
+        desingation: "Consultant",
+        description: "Enhanced the existing payment gateway for the financial network provider where monitoring, handling the transactions and scaling the application during the festive season was the main task. Along with it, I implementated push notification service for the customer in regards with the offer that they carried for their card while travelling/ staying at a particular location. "
+    },
+    {
+        id: 3,
+        img: "",
+        name: "Atos",
+        duration: "2017-2018",
+        desingation: "System Engineer - 2",
+        description: "Transformed the whole website of Indian Telecom Service provider where I handled modules like showing up the plans, recharge done through payment gateways, handling the transaction that were made during the session via wallets, card, netbanking and placing an order to CRM."
+    },
+    {
+        id: 4,
+        img: "",
+        name: "IBM India Pvt Ltd",
+        duration: "2014-2017",
+        desingation: "Web Application Developer",
+        description: "Created web components using IBM Web Content Management and it's corrosponding service using J2EE and servlets. Major contribution was made in maintaining the dashbaord of a customer having Saving Bank Account(SBA) and display information after everyday account reconciliation process."
+    }
+];
+
+export const education = [
+    {
+        id: 0,
+        img: "",
+        name: "International Institute of Information Technology, Bhubaneswar",
+        batch: "2010-2014",
+        programme: "B.Tech (Information Technology)"
+    },
+    {
+        id: 1,
+        img: "",
+        name: "Delhi Public School, Rourkela",
+        batch: "2008-2009",
+        programme: "High School - (Physics, Chemisitry, Maths)"
+    }
+];
+
+export const projects =[];
+
---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg: '#1C1C27',
+    bglight: '#1C1E27',
+    primary: '#854CE6',
+    text_primary: '#F2F3F4',
+    text_secondary: '#b1b2b3',
+    card: '#171721',
+    card_light: '#191924',
+    button: '#853CE6',
+    white: '#FFF',
+    black: '#000'
+};
+
+export const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#f0f0f0",
+  primary:"#be1adb",
+  text_primary: "#111111",
+  text_secondary: "#48494a",
+  card: "#FFF",
+  button: "#5c5b5b"
+};
