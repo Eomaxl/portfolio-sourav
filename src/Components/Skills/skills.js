1de70534a364281b8578807888053cee8a2a59ae
Sourav Mansingh <somansin@cisco.com>
2024-01-06 17:17:41 +0530

    [Portfolio] Implemented Hero Section

--- src/Components/Skills/skills.js
+++ src/Components/Skills/skills.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {skills} from '../../data/constants';
+import {Container, Wrapper, Title, Desc, SkillsContainer, Skill, SkillItem, SkillList, SkillTitle, SkillImage} from './SkillsStyledComponents';
 
-const skills = () => {
+const Skills = () => {
     return (
-        <div> Skills </div>
+        <Container id="skills">
+            <Wrapper>
+                <Title>Skill Sets</Title>
+                <Desc>The technical skills which I have acquired from past 9 years </Desc>
+                <SkillsContainer>
+                    {skills.map((skill) =>(
+                        <Skill>
+                            <SkillTitle>{skill.title}</SkillTitle>
+                            <SkillList>
+                                {skill.skills.map((item)=>(
+                                    <SkillItem>
+                                        <SkillImage src={item.image}/>
+                                        <span>{item.name}</span>
+                                    </SkillItem>
+                                ))}
+                            </SkillList>
+                        </Skill>
+                    ))}
+                </SkillsContainer>
+            </Wrapper>
+        </Container>
     )
 };
 
-export default skills;
+export default Skills;
